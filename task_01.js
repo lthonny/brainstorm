1cf4769ac75d0338710f5f6dcbd90ec2d3a8558c
lthonny <thonny02@mail.ru>
2021-06-02 17:10:08 +0300

    start

---
+++ task_01.js
@@ -0,0 +1,5 @@
+// ? https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+// * Повтор строки
+// Напишите функцию с именем repeatStr, которая повторяет заданную строку ровно n раз.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
