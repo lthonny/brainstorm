5e4ae9b0b419ecb940e0d4a6771973be3ba26c60
lthonny <thonny02@mail.ru>
2021-06-02 18:51:15 +0300

    /

--- task_01.js
+++ task_01.js
@@ -3,3 +3,22 @@
 // Напишите функцию с именем repeatStr, которая повторяет заданную строку ровно n раз.
 // repeatStr(6, "I") // "IIIIII"
 // repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr(quantity, repetitionElement) {
+	return repetitionElement.repeat(quantity);
+}
+
+const testCases = [
+	{ quantity: 6, data: 'hello' },
+	{ quantity: 3, data: 'lo' },
+	{ quantity: 7, data: 'I' },
+	{ quantity: 4, data: 'el' },
+];
+
+testCases.forEach(testCases => {
+	const { quantity, data } = testCases;
+	const repeat = repeatStr(quantity, data);
+	console.log(`
+    quantity: ${quantity}, data: ${data} -- result: ${repeat};
+  `);
+});
--- task_02.js
+++ task_02.js
@@ -1,3 +1,31 @@
 // ? https://www.codewars.com/kata/57a0885cbb9944e24c00008e
 // * Убрать восклицательные знаки
 // Напишите функцию RemoveExclamationMarks, которая удаляет все восклицательные знаки из заданной строки.
+
+function RemoveExclamationMarks(string) {
+	const characterArray = string.split('');
+	const newArray = [];
+
+	characterArray.forEach(item => {
+		if (item != '!') {
+			newArray.push(item);
+		}
+		return item;
+	});
+	return newArray.join('');
+}
+
+const testCases = [
+	{ input: '!hello!vasil!!' },
+	{ input: 'HELLO!!0!' },
+	{ input: 'asasdASs9!' },
+	{ input: 'ПРИВЕТУЛИ!' },
+];
+
+testCases.forEach(testCases => {
+	const { input } = testCases;
+	const removeCharet = RemoveExclamationMarks(input);
+	console.log(`
+    input: ${input}, result: ${removeCharet};
+  `);
+});
