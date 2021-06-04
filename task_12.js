9f30552e0d315100e97d72aad66c09e6f486ea2a
lthonny <thonny02@mail.ru>
2021-06-04 18:31:13 +0300

    in developing

--- task_12.js
+++ task_12.js
@@ -8,6 +8,38 @@
 // Решение должно быть нечувствительным к регистру (т.е. хорошее, хорошее и хорошее - все считается хорошей идеей).
 // Все входные данные не могут быть строковыми
 
-function well(x) {}
+function well(arr) {
+	const mergedArray = [].concat(...arr);
+	const bab = [];
+	for (let i = 0; i < mergedArray.length; i++) {
+		if (mergedArray[i] === mergedArray[i + 1]) {
+			bab.push(mergedArray[i]);
+		}
+	}
+	console.log(bab);
+}
+// well([2, 3, 4]);
 
-well();
+well([
+	['bad', 'bAd', 'bad'], // плохо
+	['bad', 'bAd', 'bad'],
+	['bad', 'bAd', 'bad'],
+]);
+
+// well([
+// 	['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], // Опубликовать
+// 	['bad'],
+// 	['gOOd', 'BAD'],
+// ]);
+
+// well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']]);  // Я чувствую запах серии!
+
+var randomArray = ['ball', 'ball', 'tree', 'ball', 'tree', 'bus', 'car'];
+var itemCount = {};
+
+randomArray.forEach(function (value) {
+	if (value in itemCount) itemCount[value] = itemCount[value] + 1;
+	else itemCount[value] = 1;
+});
+
+console.log(itemCount);
