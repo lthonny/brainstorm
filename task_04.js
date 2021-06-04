f61c27df9c4c395ef5d0e578e3f013919b7b4170
lthonny <thonny02@mail.ru>
2021-06-04 14:47:20 +0300

    decision-multiplicationTable

--- task_04.js
+++ task_04.js
@@ -14,3 +14,24 @@
 // 10 * 5 = 50
 // P. S. Вы можете использовать \ n в строке, чтобы перейти к следующей строке.
 // Примечание: новые строки следует добавлять между строками, но в конце не должно быть конечных символов новой строки.
+
+const n = 5;
+
+function multiplicationTable(n) {
+	let i = 1;
+	const strMultiplication = [];
+	while (i <= 10) {
+		strMultiplication.push(`${i} * ${n} = ${i * n}`);
+		i++;
+	}
+	return strMultiplication.join('\n');
+}
+
+console.log(multiplicationTable(n));
+
+// codewars
+const n = 5;
+
+const multiTable = n =>
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => `${i} * ${n} = ${i * n}`).join('\n');
+console.log(multiTable(n));
