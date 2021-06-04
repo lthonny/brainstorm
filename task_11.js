a58f93c635d2347d4fe62e4fb1f1e0e47fe1f0a4
lthonny <thonny02@mail.ru>
2021-06-04 16:55:30 +0300

    decision small enough

--- task_11.js
+++ task_11.js
@@ -4,3 +4,18 @@
 // Вы должны убедиться, что все значения в массиве ниже или равны предельному значению.
 // Если да, верните true. В противном случае верните false.
 // Вы можете считать, что все значения в массиве являются числами.
+
+function smallEnough(arr, limit) {
+	function bruteForceArr(element) {
+		return element <= limit;
+	}
+	return arr.every(bruteForceArr);
+}
+
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
+
+// сodewars
+function smallEnough(arr, limit) {
+	return Math.max(...arr) <= limit;
+}
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
