e9cda3ce0283ecf8b8eb72515690c905098a27e2
lthonny <thonny02@mail.ru>
2021-06-05 18:28:10 +0300

    desicion create a phone number

--- tesk_016.js
+++ tesk_016.js
@@ -6,3 +6,25 @@
 // createPhoneNumber ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => возвращает «(123) 456-7890»
 // Возвращенный формат должен быть правильным для выполнения этой задачи.
 // Не забывайте пробел после закрывающих скобок!
+
+function createPhoneNumber(numbers) {
+	const countryСode = numbers.splice(0, 3).join('');
+	const cityСode = numbers.splice(0, 3).join('');
+	const subscriberPhoneNumber = numbers.splice(0, 4).join('');
+	const fullNumbers = `(${countryСode}) ${cityСode}-${subscriberPhoneNumber}`;
+	return fullNumbers;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+// codewars
+
+function createPhoneNumber(numbers) {
+	const format = '(xxx) xxx-xxxx';
+
+	for (var i = 0; i < numbers.length; i++) {
+		format = format.replace('x', numbers[i]);
+	}
+
+	return format;
+}
